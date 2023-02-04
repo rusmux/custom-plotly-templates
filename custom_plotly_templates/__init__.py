892c797892686e64f96e28fc2fb29f757fd4cb05
Ruslan Mukhametshin <rusmux73@gmail.com>
2023-02-04 04:20:48 +0300

    Add pre-commit

--- custom_plotly_templates/__init__.py
+++ custom_plotly_templates/__init__.py
@@ -1,12 +1,5 @@
-__submodules__ = [
-        "templates",
-        "configs",
-]
+from custom_plotly_templates._version import __version__
+from custom_plotly_templates.configs import custom_show_config, set_custom_show_config
+from custom_plotly_templates.templates import custom_white, set_custom_template
 
-from .configs import custom_show_config, set_custom_show_config
-from .templates import custom_white, set_custom_template
-from ._version import __version__
-
-__all__ = ["__version__",
-           "custom_white", "set_custom_template",
-           "custom_show_config", "set_custom_show_config"]
+__all__ = ["__version__", "custom_white", "set_custom_template", "custom_show_config", "set_custom_show_config"]
